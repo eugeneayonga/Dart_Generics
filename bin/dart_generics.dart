2d4ee523e156db73d4068493e94c128496cc63fc
Eugene <eugeneayonga@gmail.com>
2023-02-28 20:48:36 +0300

    Imported generics examples package

--- bin/dart_generics.dart
+++ bin/dart_generics.dart
@@ -1,5 +1,11 @@
 import 'package:dart_generics/dart_generics.dart' as dart_generics;
+import 'package:dart_generics/dart_generics_examples.dart'
+    as dart_generics_examples;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_generics.calculate()}!');
+  print('*********************************************');
+
+  print('${dart_generics_examples.genericsDisplay(arguments)}');
+  print('*****************************************************');
 }
