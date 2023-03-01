e97f2def11c728ee06d5bb924e164be32fc0f1a9
Eugene <eugeneayonga@gmail.com>
2023-03-01 08:37:30 +0300

    Imported generics within classes and methods package, rendered it

--- bin/dart_generics.dart
+++ bin/dart_generics.dart
@@ -1,11 +1,16 @@
 import 'package:dart_generics/dart_generics.dart' as dart_generics;
 import 'package:dart_generics/dart_generics_examples.dart'
     as dart_generics_examples;
+import 'package:dart_generics/dart_generics_within_classes_and_methods.dart'
+    as dart_generics_within_classes_and_methods;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_generics.calculate()}!');
   print('*********************************************');
 
   print('${dart_generics_examples.genericsDisplay(arguments)}');
+  print('*****************************************************');
+
+  print('${dart_generics_within_classes_and_methods.utilsDisplay(arguments)}');
   print('*****************************************************');
 }
